c71dee46de38bf06203478fa5b8ecd7c57ee2ce8
addolfo <financieras@gmail.com>
2021-06-16 10:46:32 +0200

    .

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def git_update():
 
 @app.route('/')
 def hello():
-  return '<h1>Hello from Flask</h1><p>Continuous Deployment.</p>'
+  return '<h1>Hello from Flask</h1><p>Continuous Deployment</p>'
