9bb4c86579c6ea81292c2f737eefab30c1f639ca
magnitopic <magnitopic@gmail.com>
2021-06-21 23:06:50 +0200

    WebHook

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ import git
 
 app = Flask(__name__)
 
+@app.route('/git_update', methods=['POST'])
+def git_update():
+  repo = git.Repo('./orbe')
+  origin = repo.remotes.origin
+  repo.create_head('main', 
+  origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+  origin.pull()
+  return '', 200
+
 @app.route('/')
 def index():
   return render_template("index.html")
