e9ace89cc84a4b6114828c1fdbe2d7587dd7a543
magnitopic <magnitopic@gmail.com>
2021-06-21 22:49:24 +0200

    Commit for video

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#from py import primos
 from flask import Flask, request, render_template
 from flask.wrappers import Response
 import git
 
 app = Flask(__name__)
 
-@app.route('/git_update', methods=['POST'])
-def git_update():
-  repo = git.Repo('./orbe')
-  origin = repo.remotes.origin
-  repo.create_head('main', 
-  origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-  origin.pull()
-  return '', 200
-
 @app.route('/')
 def index():
   return render_template("index.html")
-
-#@app.route('/primos')
-#def primos():
-#  n=200
-#  return render_template("primos.html", lista=primos.generaPrimos(n))
