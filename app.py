8f022d48c4ab6b2abedf3ee08801e84e8e166d38
magnitopic <magnitopic@gmail.com>
2021-06-22 10:49:08 +0200

    Video over

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#from py import primos
 from flask import Flask, request, render_template
 from flask.wrappers import Response
 import git
@@ -16,3 +19,8 @@ def git_update():
 @app.route('/')
 def index():
   return render_template("index.html")
+
+#@app.route('/primos')
+#def primos():
+#  n=200
+#  return render_template("primos.html", lista=primos.generaPrimos(n))
---
+++ py/primos.py
@@ -0,0 +1,8 @@
+def generaPrimos(n):
+    lista=[i for i in range(2,n+1)]
+    for i, k in enumerate(lista):
+        if i<=int(n**.5):
+            for j in lista[i+1:]:
+                if j % k == 0:
+                    lista.remove(j)
+    return lista
