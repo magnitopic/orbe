752361a28af401d2dcb11ff4bd1d3359dbfbecd1
magnitopic <magnitopic@gmail.com>
2021-06-22 14:49:07 +0200

    App now in english

--- app.py
+++ app.py
@@ -3,12 +3,13 @@
 #from py import primos
 from flask import Flask, request, render_template
 from flask.wrappers import Response
-from py.primos import generaPrimos
+from py.prime import makePrime
 import git
 
 
 app = Flask(__name__)
 
+#Route for the GitHub webhook
 @app.route('/git_update', methods=['POST'])
 def git_update():
   repo = git.Repo('./orbe')
@@ -22,10 +23,10 @@ def git_update():
 def index():
   return render_template("index.html")
 
-@app.route('/primos')
+@app.route('/prime')
 def primos():
   n=200
-  return render_template("primos.html", lista=generaPrimos(n), titulo="Números primos hasta "+str(n))
+  return render_template("prime.html", list=makePrime(n), title="First "+str(n)+" prime numbers")
 
 '''if __name__=="__main__":
   app.run(debug=True)'''
--- py/prime.py
+++ py/prime.py
@@ -1,4 +1,4 @@
-def generaPrimos(n):
+def makePrime(n):
     lista=[i for i in range(2,n+1)]
     for i, k in enumerate(lista):
         if i<=int(n**.5):
