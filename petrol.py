dd2ae5cea71a80555f2c3872491b707979ff9676
magnitopic <magnitopic@gmail.com>
2022-05-03 13:03:33 +0200

    Simplificado codigo getPetrolPrice

--- petrol.py
+++ petrol.py
@@ -4,30 +4,21 @@ import json
 
 
 def getPetrolPrice(provincia, producto):
-    peticion = requests.get(
-        "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres")
+    peticion = requests.get("https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres")
     contenido = json.loads(peticion.content)
     listaEstaciones = contenido["ListaEESSPrecio"]
+    provincia = provincia.upper()
+    estacionBarata = ""
 
-    # inicializamos un diccionario. Buscamos la estación de precio menor
-    estacionBarata = {}
     for estacion in listaEstaciones:
-        if provincia.upper() == "ESPAÑA":                       # Se escribe en capitalize
-            # no todas las gasolineras tienen Biodiesel, por ejemplo, asi las quitamos
+        if provincia == estacion["Provincia"] or provincia == "ESPAÑA":
             if estacion[producto] != "":
                 if estacionBarata == {}:
                     estacionBarata = estacion
                 else:
                     if estacion[producto] < estacionBarata[producto]:
                         estacionBarata = estacion
-        # filtramos por provincia
-        elif provincia.upper() == estacion["Provincia"]:
-            if estacion[producto] != "":
-                if estacionBarata == {}:
-                    estacionBarata = estacion
-                else:
-                    if estacion[producto] < estacionBarata[producto]:
-                        estacionBarata = estacion
+
     if len(estacionBarata) == 0:
         return None
     else:
